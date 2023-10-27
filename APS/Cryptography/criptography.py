d6489125efa40bfa74e9aaff231fd1f602b27afd
cnatanae <cnatanae@student.42sp.org.br>
2023-10-27 15:29:54 -0300

    Implementation of our own cipher based on the Vignere cipher, bug fixes and addition of the feature of generating a table for the password used in our own cipher. Added color escape characters to some program messages.

--- APS/Cryptography/criptography.py
+++ APS/Cryptography/criptography.py
@@ -1,82 +1,103 @@
-'''
-Boa noite!
+import random
+import string
 
-O projeto mostrado a seguir é um exemplo desenvolvido por nós utilizando a Criptografia de César. 
+printable_chars = ''.join([chr(i) for i in range(32, 127)])
 
- Muito conhecida por sua simplicidade, possui este nome, segundo o escritor Suetônio,
- por ter sido usada por Júlio César para se comunicar com seus generais, escondendo as mensagens. 
- Sendo assim uma das primeiras formas de criptografia usadas na história.
+def generate_shuffled_characters(key):
+    seed_value = sum([ord(char) for char in key])
+    random.seed(seed_value)
+    
+    shuffled_chars = list(printable_chars)
+    random.shuffle(shuffled_chars)
+    
+    return ''.join(shuffled_chars)
 
- Os comentários serão simples e obejtivos, com o intuito de explicar de uma forma mais confortável e
-didática para os iniciantes.
+def generate_fully_shuffled_matrix(key):
+    shuffled_chars = generate_shuffled_characters(key)
+    matrix_size = len(shuffled_chars)
+    
+    matrix = []
+    for i in range(matrix_size):
+        first_char = shuffled_chars[i]
+        remaining_chars = list(shuffled_chars[:i] + shuffled_chars[i+1:])
+        random.shuffle(remaining_chars)
+        
+        row = first_char + ''.join(remaining_chars)
+        matrix.append(row)
+    
+    return matrix
 
-Cada explicação será composta por uma string de comentário, seguindo a ordem a qual cada função, variaveis, 
-métodos, serão criados ao decorrer do código do projeto.
+def cipher_with_fixed_matrix(message, key):
+    matrix = generate_fully_shuffled_matrix(key)
+    
+    ciphered_message = ""
+    for idx, char in enumerate(message):
+        key_char = key[idx % len(key)]
+        
+        row_idx = matrix[0].index(key_char)
+        col_idx = matrix[0].index(char)
+        
+        ciphered_char = matrix[row_idx][col_idx]
+        ciphered_message += ciphered_char
 
+    return ciphered_message
 
-'''
+def decipher_with_fixed_matrix(ciphered_message, key):
+    matrix = generate_fully_shuffled_matrix(key)
+    
+    deciphered_message = ""
+    for idx, char in enumerate(ciphered_message):
+        key_char = key[idx % len(key)]
+        
+        col_idx = matrix[0].index(key_char)
+        row_idx = matrix[col_idx].index(char)
+        
+        deciphered_char = matrix[0][row_idx]
+        deciphered_message += deciphered_char
 
-# Função para criptografar uma mensagem usando a cifra de César.
+    return deciphered_message
+
+def save_secret_table_to_file(key):
+    matrix = generate_fully_shuffled_matrix(key)
+    with open("secret_table.txt", "w") as file:
+        for row in matrix:
+            file.write(row + "\n")
+    print("\033[3;30;47mTabela de criptografia salva em secret_table.txt!\033[m")
+
+##
+
 def criptor_cesar(msn, key):
-    # Inicializa a string final.
     str_final = ""
-    # Percorre cada caractere na mensagem.
     for x in msn:
-        # Verifica se o caractere é uma letra.
         if x.isalpha():
-            # Define o deslocamento com base em se a letra é maiúscula ou minúscula.
             shift = 26 if x.isupper() else 26
-            # Calcula o novo valor do caractere após a aplicação da chave.
             new_value = (ord(x) - ord('A' if x.isupper() else 'a') + key) % shift
-            # Converte o novo valor para um caractere.
             new_char = chr(new_value + ord('A' if x.isupper() else 'a'))
-            # Adiciona o novo caractere à string final.
             str_final += new_char
-        # Verifica se o caractere é um número.
         elif x.isnumeric():
-            # Calcula o novo valor do número após a aplicação da chave.
             new_value = (int(x) + key) % 10
-            # Converte o novo valor para uma string.
             new_char = str(new_value)
-            # Adiciona o novo caractere à string final.
             str_final += new_char
         else:
-            # Se o caractere não for uma letra nem um número, ele é adicionado à string final sem alterações.
             str_final += x
-    # Retorna a string final após todas as transformações.
     return str_final
 
-# Função para descriptografar uma mensagem usando a cifra de César.
 def descriptor_cesar(msn, key):
-    # Inicializa a string final.
     str_final = ""
-    # Percorre cada caractere na mensagem.
     for x in msn:
-        # Verifica se o caractere é uma letra.
         if x.isalpha():
-            # Define o deslocamento com base em se a letra é maiúscula ou minúscula.
             shift = 26 if x.isupper() else 26
-            # Calcula o novo valor do caractere após a aplicação da chave.
             new_value = (ord(x) - ord('A' if x.isupper() else 'a') - key) % shift
-            # Converte o novo valor para um caractere.
             new_char = chr(new_value + ord('A' if x.isupper() else 'a'))
-            # Adiciona o novo caractere à string final.
             str_final += new_char
-        # Verifica se o caractere é um número.
         elif x.isnumeric():
-            # Calcula o novo valor do número após a aplicação da chave.
             new_value = (int(x) - key) % 10
-            # Converte o novo valor para uma string.
             new_char = str(new_value)
-            # Adiciona o novo caractere à string final.
             str_final += new_char
         else:
-            # Se o caractere não for uma letra nem um número, ele é adicionado à string final sem alterações.
             str_final += x
-    # Retorna a string final após todas as transformações.
     return str_final
 
-# Função para gerar uma senha aleatória com letras maiúsculas, minúsculas, números e caracteres especiais.
 def gen_password ():
     import random
 
@@ -86,17 +107,20 @@ def gen_password ():
     c_esp = '!@#$%&*()[]?/|.+-=_'
 
     comp = let_upper + let_lower + num + c_esp
-    digit = int(input('Digite o valor que deseja ter no tamanho da sua senha: '))
-
+    try:
+        digit = int(input('\033[3;30;47mDigite o valor que deseja ter no tamanho da sua senha:\033[m\n'))
+    except ValueError:
+                print("\033[91m\033[1mErro: Numero invalido!\033[0m\n")
+                return
     password = "".join(random.sample(comp, digit))
 
     idx = 0
     arr = []
     arr.append(password)
-    print('Sua senha é: \n', password)
+    print('Sua senha é:\n', password)
     
     while True:
-        resp = input('Deseja gerar outra senha? [s / n] ')
+        resp = input('\033[3;30;47mDeseja gerar outra senha? [s / n]\033[m\n')
         n_password = "".join(random.sample(comp, digit))
         
         if resp.lower() == 'n':
@@ -106,38 +130,97 @@ def gen_password ():
             arr.append(n_password)
             idx += 1
         else:
-            print('resposta invalida')
+            print('\033[91m\033[1mresposta invalida\033[0m\n')
     
-    print(f'As senhas que foram geradas são, respectivamente: {arr}')
-    print(f'Você gerou um total de {idx + 1} senhas')
-
-# Função para apresentar um menu para o usuário escolher entre criptografia, descriptografia e geração de senha.
+    print(f'\033[3;30;47mAs senhas que foram geradas são, respectivamente:\033[m\n{arr}\n')
+    print(f'\033[3;30;47mVocê gerou um total de {idx + 1} senhas\033[m\n')
 
 def ft_call():
     while True:
-        print("Escolha uma opção:")
-        print("1 - Criptografia de César")
-        print("2 - Descriptografia de César")
-        print("3 - Gerador de Senhas")
-        print("0 - Sair")
+        print("\n\n\n\n\033[3;30;47mEscolha uma opção: \033[m\n")
+        print("\033[3;30;47m1 - Criptografia de César \033[m\n")
+        print("\033[3;30;47m2 - Descriptografia de César \033[m\n")
+        print("\033[3;30;47m3 - Gerador de Senhas \033[m\n")
+        print("\033[3;30;47m4 - cifra secreta \033[m\n")
+        print("\033[3;30;47m5 - decifra secreta \033[m\n")
+        print("\033[3;30;47m6 - Tabela de criptografia própria \033[m\n")
+        print("\033[3;30;47m0 - Sair \033[m\n")
 
-        option = input("Digite o número da opção desejada: ")
+        option = input("\033[3;30;47mDigite o número da opção desejada:\033[m\n\n")
 
         if option == '1':
-            msn = input("Digite a mensagem a ser criptografada: ")
-            key = int(input("Digite a chave de criptografia: "))
-            msn_cripto = criptor_cesar(msn, key)
-            print("Mensagem criptografada:", msn_cripto)
+            msn = input("\033[3;30;47mDigite a mensagem a ser criptografada:\033[m\n\n")
+            if not msn.strip():
+                print("\033[91m\033[1mErro: Mensagem vazia!\033[0m\n")
+                return
+            try:
+                key = int(input("\033[3;30;47mDigite a chave de criptografia (Apenas números):\033[m\n"))
+            except ValueError:
+                print("\033[91m\033[1mErro: Chave inválida!\033[0m\n")
+                return
+            try:
+                msn_cripto = criptor_cesar(msn, key)
+                print("\033[92m\033[1mMensagem criptografada:\n" + msn_cripto + "\033[0m")
+            except Exception as e:
+                print(f"\033[91m\033[1mErro ao criptografar a mensagem:\n{e}\033[0m")
         elif option == '2':
-            msn = input("Digite a mensagem a ser descriptografada: ")
-            key = int(input("Digite a chave de descriptografia: "))
-            msn_descript = descriptor_cesar(msn, key)
-            print("Mensagem descriptografada:", msn_descript)
+            msn = input("\033[3;30;47mDigite a mensagem a ser descriptografada:\033[m\n")
+            if not msn.strip():
+                print("\033[91m\033[1mErro: Mensagem vazia!\033[0m\n")
+                return
+            try:
+                key = int(input("\033[3;30;47mDigite a chave de descriptografia (Apenas números):\033[0m\n"))
+            except ValueError:
+                print("\033[91m\033[1mErro: Chave inválida!\033[0m\n")
+                return
+            try:
+                msn_descript = descriptor_cesar(msn, key)
+                print("\033[92m\033[1mMensagem descriptografada:\n" + msn_descript + "\033[0m")
+
+            except Exception as e:
+              print(f"\033[91m\033[1mErro ao criptografar a mensagem:\n{e}\033[0m")
         elif option == '3':
             gen_password()
+        elif option == '4':
+            msn = input("\033[3;30;47mDigite a mensagem a ser criptografada:\033[0m\n")
+            if not msn.strip():
+                print("\033[91m\033[1mErro: Mensagem vazia!\033[0m")
+                return
+            try:
+                key = input("\033[3;30;47mDigite a chave de criptografia (Uma string que pode conter letras, números e simbolos especiais):\033[0m\n")
+            except ValueError:
+                print("\033[91m\033[1mErro: Chave inválida!\033[0m\n")
+                return
+            try:
+                msn_cripto = cipher_with_fixed_matrix(msn, key)
+                print("\033[92m\033[1mMensagem criptografada:\n" + msn_cripto + "\033[0m")
+            except Exception as e:
+                 print(f"\033[91m\033[1mErro ao criptografar a mensagem:\n{e}\033[0m")
+        elif option == '5':
+            msn = input("\033[3;30;47mDigite a mensagem a ser descriptografada:\033[0m\n")
+            if not msn.strip():
+                print("\033[91m\033[1mErro: Mensagem vazia!\033[0m\n")
+                return
+            try:
+                key = input("\033[3;30;47mDigite a chave de descriptografia (Uma string que pode conter letras, números e simbolos especiais):\033[0m\n")
+            except ValueError:
+                print("\033[91m\033[1mErro: Chave inválida!\033[0m\n")
+                return
+            try:
+                msn_descript = decipher_with_fixed_matrix(msn, key)
+                print("\033[92m\033[1mMensagem descriptografada:\n" + msn_descript + "\033[0m")
+            except Exception as e:
+                print(f"\033[91m\033[1mErro ao criptografar a mensagem:\n{e}\033[0m")
+        elif option == '6':
+            key = input("\033[3;30;47mDigite a chave para gerar a tabela de criptografia (Uma string que pode conter letras, números e simbolos especiais):\033[0m\n")
+            if not key.strip():
+                print("\033[91m\033[1mErro: Chave vazia!\033[0m")
+                return
+            save_secret_table_to_file(key)
         elif option == '0':
-            print("Saindo do programa.")
+            print("\033[3;30;47mSaindo do programa.\033[0m\n")
+            break
         else:
-            print("Opção inválida. Tente novamente.")
+            print("\033[91m\033[1mOpção inválida. Tente novamente.\033[0m\n")
 
 ft_call()
